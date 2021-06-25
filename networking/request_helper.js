0134ec7f907db9c4bb360c64534afb796c6af374
Dario <darioemanuelmarquez@gmail.com>
2021-06-25 20:35:32 -0300

    Clasp config added

---
+++ networking/request_helper.js
@@ -0,0 +1,23 @@
+function makeRequest(url, params, cacheKey){
+    let dataToReturn = {};
+  
+    let CACHE_KEY = cacheKey !== undefined ? cacheKey : '';
+    let cache = CacheService.getUserCache();
+    let cached = cache.get(CACHE_KEY);
+  
+    if(cached && cached !== null && cached.length > 1){
+      console.info("Getting data from cache...");
+      dataToReturn = JSON.parse(cached);
+    }else{
+      console.info(`Getting data from ${url}...`);
+
+      let response = UrlFetchApp.fetch(url, params);
+      dataToReturn = JSON.parse(response.getContentText());
+      console.log(JSON.stringify(dataToReturn));
+      
+      if (response && response.getResponseCode() == 200 && dataToReturn.length > 1 && dataToReturn.length < 99900) {
+          cache.put(CACHE_KEY, response.getContentText(), dataToReturn["caching_time"] || 60)
+      }
+    }
+    return dataToReturn;
+  }
