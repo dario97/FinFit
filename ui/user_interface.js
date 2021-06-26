9ff041813d11070b28cde6e2867147b4ccb04fb7
Dario <darioemanuelmarquez@gmail.com>
2021-06-26 11:18:18 -0300

    Added set_api_keys_form

---
+++ ui/user_interface.js
@@ -0,0 +1,19 @@
+function onOpen(){
+    let ui = SpreadsheetApp.getUi();
+
+    ui.createMenu('FinFit')
+        .addItem('Set API Keys', 'showSetAPIKeysForm')
+        .addItem('Refresh Amounts', 'refreshPortfolioAmounts')
+        .addItem('Refresh Settings', 'showRefreshConfigurationForm')
+        .addItem('Add ERC20 Token', 'showAddERC20TokenForm')
+        .addItem('Delete all properties', 'deleteProperties')
+        .addToUi();
+}
+
+function showSetAPIKeysForm(){
+    let ui = SpreadsheetApp.getUi();
+  
+    var htmlService = HtmlService.createTemplateFromFile('set_api_keys_form');
+    const html = htmlService.evaluate();
+    ui.showModalDialog(html, "Set API Keys");
+  }
